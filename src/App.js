f4c51c3449ba505f0b6a357125f9d31c1b546713
trastolillo <trastolillo@tutanota.com>
2021-07-12 05:30:58 +0200

    1.21.11

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+// Components
+import Header from './components/Header';
 
 // Styles
 import { GlobalStyle } from './GlobalStyle'
@@ -6,6 +8,7 @@ import { GlobalStyle } from './GlobalStyle'
 function App() {
   return (
     <div className="App">
+      <Header />
       Start here.
       <GlobalStyle />
     </div>
