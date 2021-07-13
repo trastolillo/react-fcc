12c109159058ae60168aaa911ed943511d83c6f8
trastolillo <trastolillo@tutanota.com>
2021-07-13 04:43:16 +0200

    2.29.45

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,33 +1,37 @@
-import React, { useState, useEffect } from 'react';
-// API
-import API from '../API';
+import React from 'react';
+
 // Config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 // Components
+import HeroImage from './HeroImage';
+import Grid from './Grid';
 
 // Hook
-
+import { useHomeFetch } from '../hooks/useHomeFetch';
 // Images
 import NoImage from '../images/no_image.jpg';
 
 const Home = () => {
-  const [state, setState] = useState();
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+  const { state, loading, error } = useHomeFetch();
+  console.log(state);
 
-  const fetchMovies = async (page, searchTerm = '') => {
-    try{
-      setError(false);
-      setLoading(true)
-
-      const movies = await API.fetchMovies(searchTerm, page)
-      console.log(movies);
-    } catch(error) {
-      setError(true)
-    }
-  };
-
-  return <div>Jompeich</div>;
+  return (
+    <>
+      {state.results[0]
+        ? <HeroImage
+          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+          title={state.results[0].original_title}
+          text={state.results[0].overview}
+        />
+        : null
+      }
+      <Grid header='Películas populares'>
+        {state.results.map(movie => (
+          <div key={movie.id}>{movie.title}</div>
+        ))}
+      </Grid>
+    </>
+  );
 };
 
 export default Home;
