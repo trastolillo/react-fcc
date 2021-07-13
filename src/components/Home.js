bfcc02b3ec4f48bc0cd69bb218a05da938e10b50
trastolillo <trastolillo@tutanota.com>
2021-07-13 17:44:47 +0200

    2.57.55

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,9 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 // Components
 import HeroImage from './HeroImage';
 import Grid from './Grid';
+import Thumb from './Thumb';
+import Spinner from './Spinner';
+import SearchBar from './SearchBar';
 
 // Hook
 import { useHomeFetch } from '../hooks/useHomeFetch';
@@ -12,7 +15,7 @@ import { useHomeFetch } from '../hooks/useHomeFetch';
 import NoImage from '../images/no_image.jpg';
 
 const Home = () => {
-  const { state, loading, error } = useHomeFetch();
+  const { state, loading, error, setSearchTerm } = useHomeFetch();
   console.log(state);
 
   return (
@@ -25,11 +28,22 @@ const Home = () => {
         />
         : null
       }
+      <SearchBar setSearchTerm={setSearchTerm} />
       <Grid header='Películas populares'>
         {state.results.map(movie => (
-          <div key={movie.id}>{movie.title}</div>
+          <Thumb
+            key={movie.id}
+            clickable
+            image={
+              movie.poster_path
+                ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+                : NoImage
+            }
+            movieId={movie.id}
+          />
         ))}
       </Grid>
+      <Spinner />
     </>
   );
 };
